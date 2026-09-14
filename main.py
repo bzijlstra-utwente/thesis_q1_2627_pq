dd5d77ee0d2a327a5f347d9cffa61f01eee64450
De-Beast11 <101040416+De-Beast11@users.noreply.github.com>
2026-09-14 15:45:53 +0200

    Added standardization

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
+import numpy as np
+import pandas as pd
+
+DATA_PATH = "data/example_data.xlsx"
+
 def main():
-    print("Hello from model-creation-code!")
+    df = pd.read_excel(DATA_PATH)
+
+    # Drop not important columns
+    df = df.iloc[:, 0:14]
+
+    # Standardize the columns of the df
+    df_z = pd.DataFrame()
+    for col_name in df.columns:
+        df_z[col_name] = (df[col_name] - df[col_name].mean()) / df[col_name].std()
+
+    
 
 
 if __name__ == "__main__":
